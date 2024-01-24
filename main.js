e074556b67ba9857bc61070fd56003c6c2b07c19
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-24 13:20:48 +0100

    finished task for disabled btn

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-function checkInputLength() {
-    var input = document.getElementById('firstNameInput');
-    var button = document.getElementById('submitButton');
 
-    if (input.value.length > 5) {
-        button.disabled = false;
-    } else {
-        button.disabled = true;
-    }
-}
+
+const firstNameInput = document.getElementById("first-name");
+const signUpButton = document.getElementById("submit");
+
+console.log("firstNameInput: ", firstNameInput);
+console.log("signUpBTN: ", signUpButton);
+
+firstNameInput.onkeyup = function (event) {
+  let firstNameInputValue = event.target.value.length;
+
+  if (firstNameInputValue >= 5) {
+    signUpButton.disabled = false;
+  } else {
+    signUpButton.disabled = true;
+  }
+};
+
+//   firstNameInputValue,length >= 5 ? signUpButton.disabled = false : signUpButton.disabled = true;
+
+
+// const input = document.getElementById('first-name');
+// const button = document.getElementById('submit');
+
+// console.log("firstNameInput: ",firstNameInput);
+// console.log("formSignUpBTN: ",formSignupBTN);
+
+// function
+
+// if (input.value.length >= 5) {
+//     button.disabled = false;
+// } else {
+//     button.disabled = true;
+// }
