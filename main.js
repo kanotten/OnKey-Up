4275bc7b0f265e098168b72c25b085339cbcb54b
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-24 12:42:34 +0100

    added form and BTN  in html

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function myfunction () {
-    alert
+function checkInputLength() {
+    var input = document.getElementById('firstNameInput');
+    var button = document.getElementById('submitButton');
+
+    if (input.value.length > 5) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
 }
